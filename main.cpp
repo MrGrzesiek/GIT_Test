cd9da58e5f0e4e3605342204acda0f4da571ba20
MrGrzesiek <mrgrzegorz2000@gmail.com>
2021-02-12 20:40:56 +0100

    Nowa metoda policz

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std:: cout <<"W pierwszym branchu; 2+3=" << (Policz2,3) <<std::endl;
+
     return 0;
+}
+Init Policz (int a, int b) {
+    Return a+b;
 }
