7d8dfcc21c0dc29cfa8624aa6d206ed60e8de8f0
MrGrzesiek <mrgrzegorz2000@gmail.com>
2021-02-12 21:03:53 +0100

    Zmiany dodatkowe w 03

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-
+init Policz (int a, int b) {
+    return (a+b)*(a-b);
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std:: cout <<"W pierwszym branchu; 2+3=" << (Policz2,3) <<std::endl;
-
+    std::cout <<"W pierwszym branchu; 2+3=" << Policz(2,3) <<
+              std::endl;
     return 0;
-}
-Init Policz (int a, int b) {
-    Return a+b;
 }
